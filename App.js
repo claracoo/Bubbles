0bc69bed2d76a971650205313ae7a9612e370cfb
Clara Cook <claracoo@buffalo.edu>
2021-11-16 12:45:48 -0500

    added on press to see further info

--- App.js
+++ App.js
@@ -1,21 +1,63 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+import React,  {useState, useEffect} from 'react';
+import Circles from './Components/Circles'
+import { View, Text, StyleSheet, Button, TouchableOpacity } from 'react-native';
+function App() {
+   const [totalRecords, setTotalRecords] = useState([])
+  const [ambigRecords, setAmbigRecords] = useState([0, 1, 2, 3, 4, 5, 6, 7, 8])
+  const [fearRecords, setFearRecords] = useState([0, 1, 2, 3])
+  const [goalsRecords, setGoalsRecords] = useState([0, 1, 2])
+  const [rundownRecords, setRundownRecords] = useState([0, 1, 2, 3, 4, 5])
+   return (
+      <View style={styles.container}>
+        <View style={styles.hello}>
+          <Text style={styles.opening}>Hello Sam,</Text>
+          <Text style={styles.opening}>What is your concern today?</Text>
+        </View>
+          <View style={styles.graph}>
+            <Circles total={totalRecords.length} ambig={ambigRecords.length} fear={fearRecords.length} goals={goalsRecords.length} rundown={rundownRecords.length}/>
+         </View>
+         <TouchableOpacity style={styles.startButton} onPress={() => {alert('You tapped the button!')}}>
+            <Text style={styles.startButtonText}> Touch Here </Text>
+          </TouchableOpacity>
+      </View>
+   );
 }
+export default App;
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const styles = StyleSheet.create({
+    container: {
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center"
+    },
+    hello: {
+      height: 100,
+      marginLeft: 15,
+      marginTop: 40,
+      marginBottom:40,
+    },
+    opening: {
+      fontWeight: "bold",
+      fontSize: 20
+    },
+    record: {
+      justifyContent: "flex-start"
+    },
+    graph: {
+      height: 200,
+      marginBottom: 80
+    },
+    startButton: {
+      width: 280,
+      height: 35,
+      textTransform: "none",
+      backgroundColor: "#3E4449",
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+      borderRadius: 4,
+    },
+    startButtonText: {
+      color: "white"
+    }
+  });
